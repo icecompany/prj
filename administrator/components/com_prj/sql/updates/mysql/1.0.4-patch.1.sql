621fee26d5a13894ee424f1709f996c163fe5bfe
xakepok@xakepok.com <44duP_yx579>
2020-04-29 14:34:52 +0300

    Version 1.1.2-patch.2

--- administrator/components/com_prj/sql/updates/mysql/1.0.4-patch.1.sql
+++
@@ -1,4 +0,0 @@
-alter table `#__mkv_projects`
-    add show_in_thematics boolean not null default 1 comment 'Показывать ли проект в списке тематик',
-    add index `#__mkv_projects_show_in_thematics_index`(show_in_thematics);
-
